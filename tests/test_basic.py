f49ae8fbbce12fda2f8fff3cff774a083322454e
Paul Tagliamonte <tag@pault.ag>
2014-04-30 16:19:42 -0400

    fix tests a bit.

--- tests/test_basic.py
+++ tests/test_basic.py
@@ -3,6 +3,7 @@
 from ssu.parser import people_to_pupa
 from contextlib import contextmanager
 import os
+import csv
 
 
 @contextmanager
@@ -12,22 +13,29 @@ def load_resource(name):
         yield fd
 
 
-def test_popolo_conversion():
+def test_john_conversion():
     with load_resource("testdata.csv") as fd:
         john = next(people_to_pupa(fd))
-        obj = john.as_dict()
-        assert obj['name'] == "John Q. Public"
-        email, address, phone = obj['contact_details']
 
-        assert email == {'value': 'ward1@example.com',
-                         'note': 'Email 1', 'type': 'email'}
+    obj = john.as_dict()
+    assert obj['name'] == "John Q. Public"
+    email, address, phone = obj['contact_details']
 
-        assert address == {'value': '123 Test Place, East North-Westchester',
-                           'note': 'Address 1', 'type': 'address'}
+    assert email == {'value': 'ward1@example.com',
+                     'note': 'Email 1', 'type': 'email'}
 
-        assert phone == {'value': '781-555-5555', 'note': 'Phone 1',
-                         'type': 'phone'}
+    assert address == {'value': '123 Test Place, East North-Westchester',
+                       'note': 'Address 1', 'type': 'address'}
 
+    assert phone == {'value': '781-555-5555', 'note': 'Phone 1',
+                     'type': 'phone'}
+
+
+def test_people_name_stream():
     with load_resource("testdata.csv") as fd:
-        for person in people_to_pupa(fd):
-            pass
+        people_stream = people_to_pupa(fd)
+        with load_resource("testdata.csv") as fd:
+            csv_stream = csv.DictReader(fd)
+
+            for person, row in zip(people_stream, csv_stream):
+                assert person.name == row['Name']
