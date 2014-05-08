e4a5390bea21e9a9795d7a22398a3c627ab1de05
Paul Tagliamonte <tag@pault.ag>
2014-05-08 17:51:22 -0400

    fix comments.

--- ssu/models.py
+++ ssu/models.py
@@ -3,32 +3,28 @@ from django.contrib.auth.models import User
 
 
 class SpreadsheetUpload(models.Model):
-    # User, when, who approved
     user = models.ForeignKey(User)
     approved_by = models.ForeignKey(User)
     created_at = models.DateTimeField()
 
 
 class SpreadsheetPerson(models.Model):
-    # name, district, person-id given
     name = models.TextField()
     district = models.TextField()
     spreadsheet = models.ForeignKey(SpreadsheetUpload)
+    # HStore here.
 
 
 class SpreadsheetAddress(models.Model):
-    # FK, address
     person = models.ForeignKey(SpreadsheetPerson)
     address = models.TextField()
 
 
 class SpreadsheetPhone(models.Model):
-    # FK, phone
     person = models.ForeignKey(SpreadsheetPerson)
     phone = models.TextField()
 
 
 class SpreadsheetEmail(models.Model):
-    # FK, email
     person = models.ForeignKey(SpreadsheetPerson)
     email = models.TextField()
